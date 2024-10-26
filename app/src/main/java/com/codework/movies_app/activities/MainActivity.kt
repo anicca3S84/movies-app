b3e6f703900db341e53ef94cb18c4f7ffbea0fef
anicca <anicca3584@gmail.com>
2024-10-26 16:45:23 +0700

    update

--- app/src/main/java/com/codework/movies_app/activities/MainActivity.kt
+++ app/src/main/java/com/codework/movies_app/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.codework.movies_app
+package com.codework.movies_app.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.codework.movies_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
